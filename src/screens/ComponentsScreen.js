a87efb532e9f9e77f952e4cf7a410d32e2dd1e86
divakarmanchala6 <divakar.manchala6@gmail.com>
2024-10-18 06:56:33 +0530

    Screen Component Created

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Text, StyleSheet} from 'react-native';
+
+const ComponentsScreen = () => {
+    return <Text style={styles.text}>This is Components Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30,
+    }
+})
+
+export default ComponentsScreen;
