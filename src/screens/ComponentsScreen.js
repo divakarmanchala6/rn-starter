cf1ca638cc09b9b4ae492b6546714599eb7d744a
divakarmanchala6 <divakar.manchala6@gmail.com>
2024-10-18 10:18:46 +0530

    first exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
-import {Text, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.text}>This is Components Screen</Text>;
+    const name = <Text style={styles.name}>My Name is Divakar</Text>
+    return (
+        <View>
+            <Text style={styles.text}>Getting Started with React Native</Text>
+            {name}
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
     text: {
-        fontSize: 30,
+        fontSize: 45,
+    },
+    name: {
+        fontSize: 20,
     }
 })
 
